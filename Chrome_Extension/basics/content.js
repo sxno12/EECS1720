164b86b39ea9471a316e687b131f31f834011c46
sxno12 <90343927+sxno12@users.noreply.github.com>
2022-01-24 02:21:50 -0500

    Break Time Reminder Popup

--- Chrome_Extension/basics/content.js
+++ Chrome_Extension/basics/content.js
@@ -1,13 +1,3 @@
 console.log("Chrome Extension Ready To Go");
 
-chrome.runtime.onMessage.addListener(gotMessage);
 
-function gotMessage(message, sender, sendResponse) {
-  console.log(message.txt);
-  if (message.txt == "hello") {
-    let paragraphs = document.getElementsByTagName("p");
-    for (elt of paragraphs) {
-      elt.style["background-color"] = "#95bab3";
-    }
-  }
-}
